8b326a75d8ff51bff0ac0b4cb3a1e7ae9c3a18ea
tontacchi <andy.tonthat14@gmail.com>
2026-04-26 10:46:22 -0700

    test: added LeafNode HTML anchor w/ props test

--- internal/ast/leafnode_test.go
+++ internal/ast/leafnode_test.go
@@ -9,8 +9,20 @@ func TestLeafToHTMLParagraph(t *testing.T) {
 	if err != nil { t.Fatalf("Unexpected error: %v", err) }
 
 	want := "<p>Hello, world!</p>"
-	if got != want {
-		t.Fatalf("got %q, want %q", got, want)
-	}
+	if got != want { t.Fatalf("got %q, want %q", got, want) }
 }
+
+func TestLeafToHTMLAnchorWithProps(t *testing.T) {
+	node := NewLeafNode(
+		"a", 
+		"Click me!",
+		map[string]string{"href": "https://www.google.com"},
+	)
+	
+	got, err := node.ToHTML()
+	if err != nil { t.Fatalf("Unexpected error: %v", err) }
+
+	want := `<a href="https://www.google.com">Click me!</a>`
+	if got != want { t.Fatalf("got %q, want %q", got, want) }
+} 
 
