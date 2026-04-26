d16851910724be9265dd79c472eaa282263691ed
tontacchi <andy.tonthat14@gmail.com>
2026-04-25 20:51:01 -0700

    feat: LeafNode defined + constructor

---
+++ internal/ast/leafnode.go
@@ -0,0 +1,16 @@
+package ast
+
+type LeafNode struct {
+	Tag   string
+	Value string
+	Props map[string]string
+}
+
+func NewLeafNode(tag, value string, props map[string]string) LeafNode {
+	return LeafNode{
+		Tag:   tag,
+		Value: value,
+		Props: props,
+	}
+}
+
